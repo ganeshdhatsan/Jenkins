e41005375c72a52caf92474de26838f2eb09e229
ganeshdhatsan <ganeshdhatsan@gmail.com>
2024-12-13 08:59:03 +0530

    code updated in runner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,18 +7,18 @@ import Utilities.TestUtilities;
 
 public class TestRunner extends TestUtilities{
 
-	LoginPageType2 loginPage2 = new LoginPageType2();
-	
-	
-	
-	@Test
-	private void validateLoginType2() throws InterruptedException {
-		browserLaunch();
-		launchUrl("https://www.facebook.com/");
-	    implicitWait();
-		loginPage2.performLogin("java", "selenium");
-	}
-	
+//	LoginPageType2 loginPage2 = new LoginPageType2();
+//	
+//	
+//	
+//	@Test
+//	private void validateLoginType2() throws InterruptedException {
+//		browserLaunch();
+//		launchUrl("https://www.facebook.com/");
+//	    implicitWait();
+//		loginPage2.performLogin("java", "selenium");
+//	}
+//	
 	
 	
 	
